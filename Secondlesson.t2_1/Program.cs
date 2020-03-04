e8316739665f627b85ca6a4f16eb64181de10732
SigStan <si.stanynaite@gmail.com>
2020-03-04 10:52:30 +0200

    Add project files.

---
+++ Secondlesson.t2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Secondlesson.t2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey, what's your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Your name is " + userName);
+
+        }
+    }
+}
