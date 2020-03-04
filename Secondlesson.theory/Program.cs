3c6db589d98e1280ca77d7b046fa302d15f8b1ce
SigStan <si.stanynaite@gmail.com>
2020-03-04 11:47:43 +0200

    example how to use operator

--- Secondlesson.theory/Program.cs
+++ Secondlesson.theory/Program.cs
@@ -6,7 +6,20 @@ namespace Secondlesson.theory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sandauga = 7 * 8;
+            Console.WriteLine("Sandauga: " + sandauga);
+
+            int dalmuo = 5 / 2;
+            Console.WriteLine("Dalmuo: " + dalmuo);
+
+            double dalmuoSuKableliu = 5/ 2;
+            Console.WriteLine("Dalmuo su kableliu: " + dalmuoSuKableliu);
+
+            int sum = 5 * 2 + 2;
+            Console.WriteLine("Suma:" + sum);
+
+            int sum2 = 5 * (2 + 2);
+            Console.WriteLine("Antra suma:" + sum2);
         }
     }
 }
